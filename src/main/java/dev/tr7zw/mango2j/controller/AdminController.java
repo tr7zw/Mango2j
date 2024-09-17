16d7dbfabd450b16c1d04e78f86ab1555261b575
tr7zw <tr7zw@live.de>
2024-09-17 20:18:16 +0200

    Utility to delete chapters, find nearly empty ones and ignore empty ones

--- src/main/java/dev/tr7zw/mango2j/controller/AdminController.java
+++ src/main/java/dev/tr7zw/mango2j/controller/AdminController.java
@@ -1,5 +1,6 @@
 package dev.tr7zw.mango2j.controller;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,17 +8,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import dev.tr7zw.mango2j.db.Chapter;
+import dev.tr7zw.mango2j.db.ChapterRepository;
+import dev.tr7zw.mango2j.db.Title;
+import dev.tr7zw.mango2j.db.TitleRepository;
 import dev.tr7zw.mango2j.jobs.FileScanner;
 import dev.tr7zw.mango2j.jobs.ThumbnailGenerator;
+import lombok.extern.java.Log;
 
 @Controller
+@Log
 public class AdminController {
 
     @Autowired
     private ThumbnailGenerator thumbnailGenerator;
     @Autowired
     private FileScanner fileScanner;
+    @Autowired
+    private TitleRepository titleRepo;
+    @Autowired
+    private ChapterRepository chapterRepo;
     
     @GetMapping("/admin/generateThumbnails")
     public ResponseEntity<String> generateThumbnails() throws IOException {
@@ -35,6 +47,18 @@ public class AdminController {
         }
         fileScanner.executeLongRunningTask();
         return new ResponseEntity<>("Ok", null, HttpStatus.OK);
+    }
+    
+    @GetMapping("/admin/delete/{id}")
+    public String deleteChapter(@PathVariable Integer id) {
+        // Add necessary attributes to the model
+        Chapter chapter = chapterRepo.getReferenceById(id);
+        Title title = titleRepo.findByFullPath(chapter.getPath());
+        File f = new File(chapter.getFullPath());
+        f.delete();
+        log.info("Deleting " + f.getAbsolutePath());
+        chapterRepo.delete(chapter);
+        return "redirect:/library/" + title.getId();
     }
     
 }
